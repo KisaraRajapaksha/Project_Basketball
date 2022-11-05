87fdd95b85601860110aa144d85acef525029cc1
KisaraNethma <nethmakisara@gmail.com>
2022-11-05 09:41:06 +0530

    Prototyped the main scene
    Prototyped the main scene and made a script to control the ball but didn't program that yet

---
+++ Project_Basketball/Assets/Scripts/Kisara/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private GameObject ball;
+    private Rigidbody2D ballRb;
+    [SerializeField] float force;
+    
+    void Awake()
+    {
+        ball = this.gameObject;
+        ballRb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            ballRb.AddForce(new Vector2(0,force), ForceMode2D.Impulse);
+        }
+    }
+}
